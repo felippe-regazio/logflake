d7abd9c8fe0682fa37bfbda7e78ab6a956ad7be6
Felippe Regazio <felippe.moraes@zoho.com>
2021-08-02 16:59:41 -0300

    added tracker testings

--- test/tracker.test.js
+++ test/tracker.test.js
@@ -21,6 +21,25 @@ describe('Test tracker', () => {
     });
   });
 
+  it('Test fnTrack', () => {
+    let hashes = [];
+    let hashA = tracker.fnTrack();
+    let hashB = tracker.fnTrack();
+
+    for(let i=0; i<10; i++) {
+      hashes.push(tracker.fnTrack());
+    }
+    
+    expect(hashes.length).toBe(10);
+    expect(hashes[0].callCount).toBe(10);
+    expect(new Set(hashes).size).toBe(1);
+    expect(hashA.callCount).toBe(1);
+    expect(hashB.callCount).toBe(1);
+    expect(hashes[0].hash).not.toEqual(hashA.hash);
+    expect(hashes[0].hash).not.toEqual(hashB.hash);
+    expect(hashA.hash).not.toEqual(hashB.hash);
+  });
+
   it('Test if track counter is right', () => {
     for(let i = 0; i<100; i++) {
       log('Testing').get((output, info) => {
@@ -39,5 +58,43 @@ describe('Test tracker', () => {
         expect(track.fnDisabled).toBe(true);
       });
     }
+  });
+
+  it('Test fnCreateHash', () => {
+    let hashes = [];
+    let hashA = tracker.fnCreateHash();
+    let hashB = tracker.fnCreateHash();
+
+    for(let i=0; i<10; i++) {
+      hashes.push(tracker.fnCreateHash());
+    }
+    
+    expect(hashes.length).toBe(10);
+    expect(new Set(hashes).size).toBe(1);
+    expect(hashes[0]).not.toEqual(hashA);
+    expect(hashes[0]).not.toEqual(hashB);
+    expect(hashA).not.toEqual(hashB);
+  });
+
+  it('Test callCountInc', () => {
+    [
+      { given: -100, expect: 1 },
+      { given: 100, expect: 101 },
+      { given: 10000000, expect: 10000001 },
+      { given: Math.MAX_SAFE_INTEGER, expect: Math.MAX_SAFE_INTEGER },
+    ].forEach(item => {
+      expect(tracker.callCountInc(item.given)).toBe(item.expect);
+    });
+  });
+
+  it('Test read & mutateFnTrack', () => {
+    let track = tracker.fnTrack();
+
+    tracker.mutateFnTrack(track.hash, { hash: 'abcdefghijklmnopqrstuv' });
+    expect(tracker.read('abcdefghijklmnopqrstuv')).toBeUndefined();
+    expect(tracker.read(track.hash)).toBeDefined();
+
+    tracker.mutateFnTrack(track.hash, { isDisabled: true });
+    expect(tracker.read(track.hash).isDisabled).toBe(true);
   });
 });
